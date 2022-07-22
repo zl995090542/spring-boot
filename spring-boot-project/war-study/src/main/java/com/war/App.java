200510651f22ec32ba4d1128681231d494366708
smartBit <995090542@qq.com>
2022-07-22 16:21:44 +0800

    docs 新增war包调试模块 fix 版本缺陷

---
+++ spring-boot-project/war-study/src/main/java/com/war/App.java
@@ -0,0 +1,24 @@
+package com.war;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+/**
+ * Hello world!
+ *
+ */
+@SpringBootApplication(scanBasePackages="com.war")
+public class App extends SpringBootServletInitializer
+{
+    public static void main( String[] args )
+    {
+        SpringApplication.run(App.class,args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(App.class);
+    }
+}
